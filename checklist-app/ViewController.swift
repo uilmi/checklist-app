b70425696d8d450e5925232f54452c908003be09
Ulul Ilmi <uilmi@na.edu>
2022-07-26 15:16:51 +0700

    init

---
+++ checklist-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checklist-app
+//
+//  Created by Ulul I. on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
